cc9c992777631eeee65b3f126ceb70a531f9db59
Ayman Mahmoud <ayman.s.mahmoud@outlook.com>
2023-08-16 08:50:26 +0400

    fixing leef

--- rosetta/rfaker.py
+++ rosetta/rfaker.py
@@ -593,6 +593,7 @@ class Events:
         faker = cls._create_faker()
         vendor = vendor or faker.company()
         version = version or faker.numerify("1.0.#")
+        event_id = faker.random_int(min=101, max=501)
         if datetime_iso is None:
             datetime_iso = datetime.now() - timedelta(hours=1)
             datetime_iso += timedelta(seconds=faker.random_int(min=0, max=3599))
@@ -604,14 +605,14 @@ class Events:
                 required_fields = "local_ip,local_port,host,url,protocol,response_code,action"
         for i in range(count):
             datetime_iso += timedelta(seconds=1)
-            leef_message = f"LEEF:1.0|{vendor}|{product}|{version}|deviceEventDate={datetime_iso}|" \
-                           f"severity={cls.set_field('severity', observables)}"
+            leef_message = f"LEEF:1.0|{vendor}|{product}|{version}|{event_id}| |" \
+                           f"severity={cls.set_field('severity', observables)} devtime={datetime_iso}"
             for field in required_fields.split(","):
-                leef_message += f" | {field}={cls.set_field(field, observables)}"
+                leef_message += f" {field}={cls.set_field(field, observables)}"
             if observables:
                 for observable, observable_value in vars(observables).items():
                     if observable_value and observable not in required_fields.split(","):
-                        leef_message += f" | {observable}={random.choice(observable_value)}"
+                        leef_message += f" {observable}={random.choice(observable_value)}"
             leef_messages.append(leef_message)
         return leef_messages
 
